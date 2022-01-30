550bf729d93955258f44871e2ee97a1486120cb5
Daniel Barton <bartondhb@gmail.com>
2022-01-30 01:41:11 -0500

    updated descriptions

--- summarize.go
+++ summarize.go
@@ -19,9 +19,9 @@ func summarize(e *[]Entity) {
 
 	p.Printf("As of the 2020 U.S. Census (all values rounded to nearest whole number):\n\n")
 
-	p.Printf("The total population of the United States is %d.\n", int64(math.Round(calcTotal(statePopulations))))
-	p.Printf("The mean population of the United States is %d.\n", int64(math.Round(calcMean(statePopulations))))
-	p.Printf("The median population of the United States is %d.\n", int64(math.Round(calcMedian(statePopulations))))
-	p.Printf("The standard deviation of the population of the United States is %d.\n", int64(math.Round(calcStdDev(statePopulations))))
+	p.Printf("The TOTAL population of the United States is %d.\n", int64(math.Round(calcTotal(statePopulations))))
+	p.Printf("The MEAN population of States and Territories within the United States is %d.\n", int64(math.Round(calcMean(statePopulations))))
+	p.Printf("The MEDIAN population of States and Territories within the United States is %d.\n", int64(math.Round(calcMedian(statePopulations))))
+	p.Printf("The STANDARD DEVIATION of States and Territories within the United States is %d.\n", int64(math.Round(calcStdDev(statePopulations))))
 
 }
